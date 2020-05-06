3d93c706356e74f2b5b75ce8ce135ef84194d67d
Justin Pothoof <jpothoof@uw.edu>
2020-05-06 15:59:46 -0700

    Initial function to calculate and output first and second derivative of xanes spectra.

---
+++ PredictXANES/xanes_derivatives.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def xanes_derivatives(mu_df, dx=0.80808080808):
+    if 'CN' in mu_df.columns:
+        mu_df = mu_df.drop(labels='CN', axis=1)
+    else:
+        pass
+
+    dmu1 = np.zeros([len(mu_df), len(mu_df.columns)])
+    dmu2 = np.zeros([len(mu_df), len(mu_df.columns)])
+
+    for i in range(len(mu_df)):
+        for j in range(len(mu_df.columns)):
+            if j == len(mu_df.columns)-1:
+                pass
+            else:
+                dmu1[i, j] = (mu_df.iloc[i, j+1] - mu_df.iloc[i, j]) / dx
+
+    for i_ in range(len(mu_df)):
+        for j_ in range(len(mu_df.columns)):
+            if j_ == len(mu_df.columns)-1:
+                pass
+            else:
+                dmu2[i_, j_] = (dmu1[i_, j_+1] - dmu1[i_, j_]) / dx
+
+    return dmu1, dmu2
